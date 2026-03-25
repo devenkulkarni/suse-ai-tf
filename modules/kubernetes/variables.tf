saurabhsadhalesuse/suse-ai-deploy

added new variable for acme server

modules/kubernetes/variables.tf
@@ -70,3 +70,9 @@ variable "gpu_operator_ns" {
   description = "Namespace for the NVIDIA GPU operator"
   default     = "gpu-operator"
 }
+
+variable "letsencrypt_acme_server" {
+  type        = string
+  description = "Acme server"
+  value       = "https://acme-staging-v02.api.letsencrypt.org/directory"
+}

devenkulkarni/suse-ai-tf

modules/kubernetes/variables.tf
@@ -83,3 +83,9 @@ variable "longhorn_chart_version" {
   default     = "v1.9.2"
 >>>>>>> 4d6b80c (LH implementation testing WIP)
 }
+
+variable "letsencrypt_acme_server" {
+  type        = string
+  description = "Acme server"
+  default     = "https://acme-staging-v02.api.letsencrypt.org/directory"
+}